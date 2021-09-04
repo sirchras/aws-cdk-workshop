9a6994db976d20945d30c350eb2bc8f48f74346b
sirchras <cjlynch@hotmail.co.nz>
2021-09-04 15:08:15 +1200

    expose cloudformation output

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,11 +1,14 @@
-import { Construct, Stack, StackProps } from '@aws-cdk/core'
+import { Construct, Stack, StackProps, CfnOutput } from '@aws-cdk/core'
 import * as lambda from '@aws-cdk/aws-lambda'
 import * as apigw from '@aws-cdk/aws-apigateway'
-// import { TableViewer } from 'cdk-dynamo-table-viewer'
+import { TableViewer } from 'cdk-dynamo-table-viewer'
 
 import { HitCounter } from './hitcounter'
 
 export class CdkWorkshopStack extends Stack {
+  public readonly hcEndpoint: CfnOutput;
+  // public readonly hcViewerURL: CfnOutput;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
@@ -19,15 +22,22 @@ export class CdkWorkshopStack extends Stack {
       downstream: hello
     })
 
-    new apigw.LambdaRestApi(this, 'Endpoint', {
-      // handler: hello
+    const gateway = new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler
     })
 
-    // new TableViewer(this, 'ViewHitCounter', {
+    // const tv = new TableViewer(this, 'ViewHitCounter', {
     //   title: 'Hello Hits',
     //   table: helloWithCounter.table,
     //   sortBy: '-hits'
+    // })
+
+    this.hcEndpoint = new CfnOutput(this, 'GatewayURL', {
+      value: gateway.url
+    })
+
+    // this.hcViewerURL = new CfnOutput(this, 'TableViewerURL', {
+    //   value: tv.endpoint
     // })
   }
 }
