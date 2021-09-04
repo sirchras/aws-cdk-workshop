64c6d0b4f7424acff19ec9746c04cba6c15ea8b1
sirchras <cjlynch@hotmail.co.nz>
2021-09-04 13:53:11 +1200

    create pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,12 +1,33 @@
 import * as cdk from '@aws-cdk/core';
 import * as codecommit from '@aws-cdk/aws-codecommit';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as actions from '@aws-cdk/aws-codepipeline-actions';
+import { SimpleSynthAction, CdkPipeline } from '@aws-cdk/pipelines';
 
 export class WorkshopPipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new codecommit.Repository(this, 'WorkshopRepo', {
+    const repo = new codecommit.Repository(this, 'WorkshopRepo', {
       repositoryName: 'WorkshopRepo'
+    });
+
+    const sourceArtifact = new codepipeline.Artifact();
+    const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+    const pipeline = new CdkPipeline(this, 'Pipeline', {
+      pipelineName: 'WorkshopPipeline',
+      cloudAssemblyArtifact,
+      sourceAction: new actions.CodeCommitSourceAction({
+        actionName: 'CodeCommit',
+        output: sourceArtifact,
+        repository: repo
+      }),
+      synthAction: SimpleSynthAction.standardNpmSynth({
+        sourceArtifact,
+        cloudAssemblyArtifact,
+        buildCommand: 'npm run build'
+      })
     });
   }
 }
